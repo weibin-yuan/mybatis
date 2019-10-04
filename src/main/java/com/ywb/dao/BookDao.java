774c014695aa5d79f57a3a2d3f4c845c91359224
weibin-yuan <1355074428@qq.com>
2019-10-04 17:26:25 +0800

    mybatis学习

--- src/main/java/com/ywb/dao/BookDao.java
+++ src/main/java/com/ywb/dao/BookDao.java
@@ -7,15 +7,17 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.InputStream;
 
 public class BookDao {
-    public  Book getBookById(int i) {
-        //使用类加载器加载mybatis的配置文件
-        InputStream inputStream = BookDao.class.getClassLoader().getResourceAsStream("config.xml");
-        //构建sqlSession的工厂
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    //使用类加载器加载mybatis的配置文件
+    InputStream inputStream = BookDao.class.getClassLoader().getResourceAsStream("config.xml");
+    //构建sqlSession的工厂
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    public  Book getBookById(Integer id) {
         //创建能执行映射文件中的sql
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try {
-            Book book = sqlSession.selectOne("com.ywb.dao.BookMapper.selectBookById", 2);
+            // 操作CRUD，第一个参数：指定statement，规则：命名空间+“.”+statementId
+            // 第二个参数：指定传入sql的参数：这里是用户id
+            Book book = sqlSession.selectOne("com.ywb.dao.BookMapper.selectBookById", id);
             return book;
         }catch(Exception e) {
             e.printStackTrace();
@@ -27,6 +29,39 @@ public class BookDao {
         }
         return null;
     }
+    public void del(Integer id) {
+        //创建能执行映射文件中的sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try{
+            sqlSession.selectOne("com.ywb.dao.BookMapper.del", id);
+        }finally {
+            if(sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+    public void add(Book book) {
+        //创建能执行映射文件中的sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try{
+            sqlSession.selectOne("com.ywb.dao.BookMapper.add", book);
+        }finally {
+            if(sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+    public void update(Book book) {
+        //创建能执行映射文件中的sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try{
+            sqlSession.selectOne("com.ywb.dao.BookMapper.update", book);
+        }finally {
+            if(sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
     public static void main(String[] args) {
         BookDao bookDao=new BookDao();
         long start=System.currentTimeMillis();
@@ -36,6 +71,16 @@ public class BookDao {
         long end=System.currentTimeMillis();
         System.out.println(end-start);
 //        System.out.println(bookDao.getBookById(1));
+        bookDao.del(1);
+
+        Book book = new Book();
+        book.setName("小明");
+        book.setNumber(456);
+        bookDao.add(book);
+
+        Book book1 = new BookDao().getBookById(2);
+        book1.setName("小红");
+        bookDao.update(book1);
 
     }
 }
--- src/main/java/com/ywb/dao/BookMapper.java
+++ src/main/java/com/ywb/dao/BookMapper.java
@@ -4,4 +4,7 @@ import com.ywb.entity.Book;
 
 public interface BookMapper {
     Book selectBookById();
+    void del();
+    void update();
+    void add();
 }
